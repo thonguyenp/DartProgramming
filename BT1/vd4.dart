ee1a9b0aafd4a02550575915e2b6c23d3eb9f7c3
thonguyenp <nphuoctho2406@gmail.com>
2025-07-22 10:51:54 +0700

    Hoàn thành vd4 về const và final

---
+++ BT1/vd4.dart
@@ -0,0 +1,16 @@
+
+void main ()
+{
+  final name = "Tho"; //bien final chi gan gia tri 1 lan
+  // name = "Nguyen";  //loi
+
+  final int age;
+
+  age = 21; //final cho phep gan MOT lan
+
+  const color = "red";  //const buoc phai khai bao ket hop gan
+
+  const int material = 1;
+
+  // print(age);
+}
