8fd1cb6e1ea64fa3dcd4183c946bf1f500d38a6d
thonguyenp <nphuoctho2406@gmail.com>
2025-07-23 13:04:36 +0700

    bt2: toán tử và phép tính cơ bản

---
+++ BT2/vd1.dart
@@ -0,0 +1,17 @@
+void main ()
+{
+  // cac phep + - * / %
+  // Chia lay phan nguyen
+  int f = 7 ~/ 2;
+  // print(f);
+  //toan tu 2 ngoi += -= *= /=
+  f~/=3;
+  // print(f);
+  //toan tu 1 ngoi a++ ++a a-- --a
+  int a = 5;
+  print(a++);
+  print(++a);
+  print(a--);
+  print(--a);
+  print(a);
+}
